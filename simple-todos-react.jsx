c6f4f566522699ae161db62fed84899bc7768562
Cory <crbi2550@gmail.com>
2015-10-18 17:38:00 -0700

    Privacy settting to make tasks public or private, only task owners can see private tasks

--- simple-todos-react.jsx
+++ simple-todos-react.jsx
@@ -7,9 +7,24 @@ if (Meteor.isClient) {
     passwordSignupFields: "USERNAME_ONLY"
   });
 
+  // Subscribes to all data from the publication "tasks"
+  Meteor.subscribe("tasks");
+
   Meteor.startup(function() {
     // Use Meteor.startup to render the component after the page is ready
     React.render(<App />, document.getElementById("render-target"));
+  });
+}
+
+if (Meteor.isServer) {
+  // Creates a publication named "tasks"
+  Meteor.publish("tasks", function() {
+    return Tasks.find({
+      $or: [
+        { private: {$ne: true} },
+        { owner: this.userId }
+      ]
+    });
   });
 }
 
@@ -29,10 +44,30 @@ Meteor.methods({
   },
 
   removeTask(taskId) {
+    const task = Tasks.findOne(taskId);
+    if (task.private && task.owner !== Meteor.userId()) {
+      throw new Meteor.error("Not Authorized");
+    }
+
     Tasks.remove(taskId);
   },
 
   setChecked(taskId, setChecked) {
+    const task = Tasks.findOne(taskId);
+    if (task.private && task.owner !== Meteor.userId()) {
+      throw new Meteor.error("Not Authorized");
+    }
+
     Tasks.update(taskId, { $set: { checked: setChecked} });
+  },
+
+  setPrivate(taskId, setToPrivate) {
+    const task = Tasks.findOne(taskId);
+
+    if (task.owner !== Meteor.userId()) {
+      throw new Meteor.error("Not Authorized");
+    }
+
+    Tasks.update(taskId, { $set: { private: setToPrivate } });
   }
 });
